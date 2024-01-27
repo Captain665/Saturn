e8cc435388161df20d84d780a732b91bbd072702
loharmanish <loharmanish9571@gmail.com>
2024-01-27 11:58:07 +0530

    fixed content width

--- src/App/Layout/Header.jsx
+++ src/App/Layout/Header.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { memo } from "react";
 import { NavLink } from "react-router-dom";
 
-import { FaUser,FaHouse, FaPhone, FaInfo } from "react-icons/fa6";
+import { FaUser,FaHouse, FaPhone, FaInfo, FaBars } from "react-icons/fa6";
 
 
 function Headers({name}) {
@@ -19,18 +19,21 @@ function Headers({name}) {
     return (
         <>
             <header className="flex w-full flex-row justify-between items-center shadow-lg bg-rose-200">
-                <NavLink to="/" className="flex justify-center w-2/6">
-                    <img src="/LogoImage.jpg" alt="logo" className="p-3 h-20 object-content ml-20 rounded-2xl" />
+                <NavLink to="/" className="flex items-center">
+                    <img src="/LogoImage.jpg" alt="logo" className="p-3 h-20 object-content ml-4 md:ml-20 rounded-2xl" />
                 </NavLink>
-                <nav className="flex w-1/2 items-center justify-around place-content-center">
+                <nav className="hidden md:flex w-1/2 items-center justify-around place-content-center flex-col md:flex-row">
                     <NavLink to="/" end className={(isActive) => isActiveCheck(isActive)}>{home}</NavLink>
                     <NavLink to="about" className={(isActive) => isActiveCheck(isActive)}>{about}</NavLink>
                     <NavLink to="contact" className={(isActive) => isActiveCheck(isActive)}>{contact}</NavLink>
                     <NavLink to="account" className={(isActive) => isActiveCheck(isActive)}><p className="flex gap-1 items-center"><FaUser/> {account}</p></NavLink>
+                </nav>
+                <nav className="md:hidden mr-10">
+                <FaBars className="text-3xl"/>
                 </nav>
             </header>
         </>
     )
 }
 
-export default Headers;
+export default memo(Headers);
