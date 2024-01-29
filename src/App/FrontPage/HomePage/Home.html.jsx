ea35197428d43c2dd0383e7861f31184e16217fe
loharmanish <loharmanish9571@gmail.com>
2024-01-29 19:57:13 +0530

    change home page image

--- src/App/FrontPage/HomePage/Home.html.jsx
+++ src/App/FrontPage/HomePage/Home.html.jsx
@@ -9,7 +9,7 @@ export default function HomePage({ pnr,handleOnChange, handleOnClick,isLoading,e
 
     return (
         <>
-            <div className="bg-cover bg-center opacity-90 bg-[url('https://burst.shopifycdn.com/photos/flatlay-iron-skillet-with-meat-and-other-food.jpg?width=1000&format=pjpg&exif=0&iptc=0')] h-svh bg-no-repeat w-full">
+            <div className="bg-cover bg-center opacity-90 bg-[url('https://www.prideofindia.co/cdn/shop/articles/Best-Cuisines_800x.jpg?v=1690289606')] h-svh bg-no-repeat w-full">
                 <div className="h-screen flex justify-center items-center flex-col">
                     <form className="self-center shadow-xl rounded-xl items-center place-content-center bg-gray-200 md:p-10 p-5 flex flex-col md:gap-2 md:flex-row" method="post">
                         <input type="number" name="pnr" maxLength="10" value={pnr} onChange={handleOnChange}
