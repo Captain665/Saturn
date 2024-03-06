3582c4e8b8a4d6832785144f5a2c91869fefc8bf
loharmanish <loharmanish9571@gmail.com>
2024-03-06 15:52:33 +0530

    add fix

--- setupProxy.js
+++ setupProxy.js
@@ -1,7 +1,7 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 export default function (app) {
     app.use(
-        '/api',
+        '/api/v2',
         createProxyMiddleware({
             target: 'https://57fd-182-71-79-227.ngrok-free.app',
             changeOrigin: true,
