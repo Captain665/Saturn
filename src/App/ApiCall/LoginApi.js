3d4b551e459af2b56b629fe68537dfa775abb487
loharmanish <loharmanish9571@gmail.com>
2024-03-10 13:36:17 +0530

    added url for login page

--- src/App/ApiCall/LoginApi.js
+++ src/App/ApiCall/LoginApi.js
@@ -9,7 +9,7 @@ export async function LoginResponse(loginData){
         body: JSON.stringify(loginData)
     };
 
-    const url = "/auth/login"
+    const url = "https://673a-2409-40d0-102f-3aa2-60f0-eab1-ea54-83f0.ngrok-free.app/api/v2/auth/login"
     const response = await fetch(url, payload);
     const jsonData = await response.json();
 
