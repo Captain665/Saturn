d60bc22ff68bfb27c8099bfb5d419708dd952fa8
loharmanish <loharmanish9571@gmail.com>
2024-04-27 12:19:38 +0530

    added toast

--- src/App/Components/MessageToggle.jsx
+++ src/App/Components/MessageToggle.jsx
@@ -1,8 +1,17 @@
 import React, { memo, useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
+import { Bounce, Flip, Slide, ToastContainer, Zoom, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
- function ErrorToster({ props }) {
+const contextClass = {
+    success: "bg-blue-600",
+    error: "bg-red-600",
+    info: "bg-gray-600",
+    warning: "bg-orange-400",
+    default: "bg-indigo-600",
+    dark: "bg-white-600 font-gray-300",
+};
+
+function ErrorToster({ props }) {
 
     console.log("Error toaster running...")
 
@@ -15,9 +24,9 @@ import 'react-toastify/dist/ReactToastify.css';
         const fail = "failure"
 
         if (status === fail) {
-            toast.error(msg, { autoClose: 4000 })
+            toast.error(msg, { autoClose: 3000 })
         } else {
-            toast.success(result, { autoClose: 4000 })
+            toast.success(result, { autoClose: 3000 })
         }
 
     }, [props])
@@ -25,7 +34,20 @@ import 'react-toastify/dist/ReactToastify.css';
 
     return (
         <div>
-            <ToastContainer />
+            <ToastContainer
+                autoClose={1000}
+                position="bottom-center"
+                theme="colored"
+                pauseOnHover={false}
+                transition={Zoom}
+                icon={false}
+                closeButton={false}
+                stacked={true}
+                hideProgressBar={true}
+                toastClassName={(context) =>
+                    contextClass[context?.type || "default"] + " relative flex p-1 rounded-md justify-between overflow-hidden cursor-pointer"}
+                bodyClassName={() => "text-lg font-white font-med block p-2 m-auto text-center"}
+            />
         </div>
     )
 
