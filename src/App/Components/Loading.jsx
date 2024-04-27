67456a427315498bb9b1ea1672b1b7662564ede0
loharmanish <loharmanish9571@gmail.com>
2024-04-27 08:03:17 +0530

    changes loader color

--- src/App/Components/Loading.jsx
+++ src/App/Components/Loading.jsx
@@ -8,7 +8,7 @@ export default function IsLoading({ isLoading }) {
         <>
             <dialog open className="flex m-auto h-full bg-transparent justify-center">
                 <ul className="w-fit h-fit flex m-auto justify-end bg-white text-xl items-center gap-2 px-5 p-2">
-                    <FaCircleNotch className="animate-spin text-gray-500" />
+                    <FaCircleNotch className="animate-spin text-black" />
                     <li className="text-normal">Loading...</li>
                 </ul>
             </dialog>
